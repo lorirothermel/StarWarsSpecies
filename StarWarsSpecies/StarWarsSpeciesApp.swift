8610db5abdf41d47ad1635d3190267e4d1f50935
lorirothermel <lori@rothermel1.com>
2024-09-19 20:43:57 -0400

    Initial Commit

---
+++ StarWarsSpecies/StarWarsSpeciesApp.swift
@@ -0,0 +1,17 @@
+//
+//  StarWarsSpeciesApp.swift
+//  StarWarsSpecies
+//
+//  Created by Lori Rothermel on 9/19/24.
+//
+
+import SwiftUI
+
+@main
+struct StarWarsSpeciesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
